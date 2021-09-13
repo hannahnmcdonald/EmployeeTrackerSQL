3a4120f879aba04d66092d95ee94baaede3ed9ed
Hannah McDonald <hannahnmcdonald@protonmail.com>
2021-09-12 21:36:06 -0400

    markup of choice functions, basic outline of work to be done

--- server.js
+++ server.js
@@ -53,8 +53,9 @@ function init() {
             'View all employees', 
             'Add a department', 
             'Add a role', 
+            'Add employee',
             'Update employee role',
-            'Quit' ]
+            'Exit' ]
     }])
     .then(userInput)
     // TODO: Functions per userInput
@@ -64,10 +65,44 @@ function init() {
     // viewEmployees();
     // addDept();
     // addRole();
+    // addEmployee();
     // updateEmployee();
-    // quitProgram();
+    // exitProgram();
 
 };
 
+viewDepts() {
+    // Show dept names + Ids
+};
+
+viewRoles() {
+    // Show job title, role id, department + salary
+};
+
+viewEmployees() {
+    // Show employee id, first name, last name, job title, department, sallaries, + managers
+};
+
+addDept() {
+    // Enter department name + adds to db
+};
+
+addRole() {
+    // Enter role name, salary + department then adds to db
+};
+
+addEmployee() {
+    // Enter employee id, first name, last name, salary, dept, + manager then add to db
+}
+
+updateEmployee() {
+    // Enter new employee role, then add to db
+}
+
+exitProgram() {
+    // Exit program
+};
 // Function to initialize application
 init();
+
+
