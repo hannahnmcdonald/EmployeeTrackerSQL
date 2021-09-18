31e4025e87276ed12367f1f6df7c008be2abfeee
unknown <hannahnmcdonald@protonmail.com>
2021-09-18 10:39:43 -0400

    fixed connection error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,8 @@
 USE employees;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Michael', 'Scott', 1, 1),
+VALUES 
+        ('Michael', 'Scott', 1, 1),
         ('Jim', 'Halpert', 3, 1),
         ('Dwight', 'Schrute', 2, 1),
         ('Andy', 'Bernard', 3, 1),
@@ -9,14 +10,16 @@ VALUES ('Michael', 'Scott', 1, 1),
         ('Kevin', 'Malone', 5, 1);
     
 INSERT INTO role (title, salary, department_id)
-VALUES ('Manager', 100000, 1),
+VALUES 
+        ('Manager', 100000, 1),
         ('Assistant Manager', 85000, 2),
         ('Sales Representative', 65000, 3),
         ('Secretary', 45000, 4),
         ('Accounting', 65000, 5);
 
 INSERT INTO department (department_name) 
-VALUES  ("Manager"),
+VALUES  
+        ("Manager"),
         ("Assistant Manager"),
         ("Sales Representative"),
         ("Secretary");
--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
-const consoleTable = require('console.table');
+const { printTable } = require('console-table-printer');
 require('dotenv').config();
 
 // Initialize App w/Express
 const app = express();
 
 // Create Port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
 // Parse incoming JSON data
 app.use(express.json());
@@ -18,33 +18,31 @@ app.use(express.urlencoded({
 }));
 
 // Connects to database + .env for password privacy
-const connection = mysql.createConnection({
+const connection = mysql.createConnection(
+    {
         host: "localhost",
         user: "root",
         password: process.env.DB_PASS,
         port: PORT,
-        database: "employee_tracker",
+        database: "employees",
         // Added connectTimeout due to frequent ._handleTimeoutError
         connectTimeout: 300000
     },
     console.log("Connected to Employee Tracker")
 );
 
-// Check for connection errors
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ', err);
-      return;
-    }
-  });
 
-//  Listening
-app.listen(PORT, () =>
-  console.log(`Employee Tracker application listening at http://localhost:${PORT}`)
-);
+// Check for connection errors
+connection.connect(function (err) {
+    if (err) {
+        console.log("unable to connect")
+    };
+    console.log('Connected to Employee Database');
+    init();
+});
 
 // Init fx
-function init() {
+function init(connection) {
     // Prompt User w/Inquirer package
     inquirer.prompt([{
     // Options to begin program
@@ -65,31 +63,31 @@ function init() {
         switch(userInput.options) {
             // Calls viewDept function
             case ('View all departments'): viewDepts();
-            console.log('View departments')
+            // console.log('View departments')
             break;
             // Calls viewRoles function
             case ('View all roles'): viewRoles();
-            console.log('View roles')
+            // console.log('View roles')
             break;
             // Calls viewEmployees function
             case ('View all employees'): viewEmployees();
-            console.log('View employees')
+            // console.log('View employees')
             break;
             // Calls addDept function
             case ('Add a department'): addDept();
-            console.log('Add department')
+            // console.log('Add department')
             break;
             // Calls addRole function
             case ('Add a role'): addRole();
-            console.log('Add role')
+            // console.log('Add role')
             break;
             // Calls addEmployee function
             case ("Add employee"): addEmployee();
-            console.log('Add employee')
+            // console.log('Add employee')
             break;
             // Calls updateEmployee function
             case ('Update employee role'): updateEmployee();
-            console.log('Update employee role')
+            // console.log('Update employee role')
             break;
             // Calls exitProgram function
             case ('Exit'): 
@@ -101,38 +99,34 @@ function init() {
     
 };
 
+
 function viewDepts() {
     // Expected Behavior: Show dept names + Ids
-    connection.query('SELECT FROM department', (err, res) => {
-        if (err) {
-            console.log(err)
-        }
-        console.log("Viewing all departments");
-        console.table(res);
+    // console.log("Viewing all departments");
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        printTable(res);
         init();
     })
 };
 
 function viewRoles() {
     // Expected Behavior: Show job title, role id, department + salary
-    connection.query('SELECT FROM roles', (err, res) => {
-        if (err) {
-            console.log(err)
-        }
-        console.log("Viewing all roles");
-        console.table(res);
+    // console.log("Viewing all roles");
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+        printTable(res);
         init();
     })
 };
 
 function viewEmployees() {
     // Expected Behavior: Show employee id, first name, last name, job title, department, sallaries, + managers
-    connection.query('SELECT FROM employees', (err, res) => {
-        if (err) {
-            console.log(err)
-        }
-        console.log("Viewing all employees");
-        console.table(res);
+    // console.log("Viewing employees");
+    connection.query("SELECT * FROM employee", (err, res) => {
+        if (err) throw err;
+        // console.log("Viewing all employees");
+        printTable(res);
         init();
     })
 };
@@ -145,15 +139,15 @@ function addDept() {
         message: "What is the new department name?",
     })
     .then((answer) => {
-            let department_name = answer.departmentName;
+            let departmentName = answer.departmentName;
 
             connection.query(
-            `INSERT INTO department (department_name) VALUES ("${department_name}")`,
+            `INSERT INTO department (department_name) VALUES ("${departmentName}")`,
             (err, res) => {
                 if (err) {
                     console.log(err);
                 }
-                console.log(`${department_name} has been added to departments!`);
+                console.log(`${departmentName} has been added to departments!`);
             }
         );
 
@@ -161,135 +155,133 @@ function addDept() {
     })
 };
 
-function addRole() {
-    // Expected Behavior: Enter role name, salary + department then add to db
-    connection.query('SELECT FROM department', (err, res) => {
-        if (err) {
-            console.log(err)
-        } else {
-            return inquirer.prompt([
-                {
-                    type: "input",
-                    name: "role",
-                    message: "Enter a new role",
-                },
-                {
-                    type: "input",
-                    name: "salary",
-                    message: "Enter the salary for this role",
-                },
-                {
-                    type: "list",
-                    name: "department",
-                    message: "What department is this role in?",
-                    choices: () => {
-                        let departmentArray = [];
-                        for (let i = 0; i < res.length; i++) {
-                            departmentArray.push(res[i].dept_name + " | " + res[i].id);
-                        }
-                        return departmentArray;
-                    },
-                },
-            ])
-            .then(function (choice) {
-                let dept = choice.department.split("|")[i];
+// function addRole() {
+//     // Expected Behavior: Enter role name, salary + department then add to db
+//     connection.query("SELECT * FROM department", (err, res) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             return inquirer.prompt([
+//                 {
+//                     type: "input",
+//                     name: "role",
+//                     message: "Enter a new role",
+//                 },
+//                 {
+//                     type: "input",
+//                     name: "salary",
+//                     message: "Enter the salary for this role",
+//                 },
+//                 {
+//                     type: "list",
+//                     name: "department",
+//                     message: "What department is this role in?",
+//                     choices: () => {
+//                         let departmentArray = [];
+//                         for (let i = 0; i < res.length; i++) {
+//                             departmentArray.push(res[i].dept_name + " | " + res[i].id);
+//                         }
+//                         return departmentArray;
+//                     },
+//                 },
+//             ])
+//             .then(function (choice) {
+//                 let dept = choice.department.split("|")[i];
 
-                connection.query(
-                    `INSERT INTO employee (title, department_id, salary) VALUES ("${choice.role}", ${dept}, "${choice.salary}") `,
+//                 connection.query(
+//                     `INSERT INTO employee (title, department_id, salary) VALUES ("${choice.role}", ${dept}, "${choice.salary}") `,
 
-                    (err) => {
-                        if (err) {
-                            console.log(err)
-                        }
-                    console.log(`"${choice.role}" added successfully!`);
-                    init();
-                    }
-                )
-            })
-        }
-    })
-};
+//                     (err) => {
+//                         if (err) {
+//                             console.log(err)
+//                         }
+//                     console.log(`"${choice.role}" added successfully!`);
+//                     init();
+//                     }
+//                 )
+//             })
+//         }
+//     })
+// };
 
-function addEmployee() {
-    // Expected Behavior: Enter employee id, first name, last name, salary, dept, + manager then add to db
-    connection.query('SELECT FROM roles', (err, res) => {
-        if (err) {
-            console.log(err)
-        } else {
-            return inquirer.prompt([
-                {
-                    type: "input",
-                    name: "firstName",
-                    message: "Enter the employee's first name",
-                    validate: (answer) => {
-                        if (answer !== "") {
-                            return true;
-                        } else {
-                    return "First name cannot be blank";
-                        }
-                    }
-                },
-                {
-                    type: "input",
-                    name: "lastName",
-                    message: "Enter the employee's last name",
-                    validate: (answer) => {
-                        if (answer === "") {
-                            return true;
-                        } else {
-                    return "Last name cannot be blank";
-                        }
-                    }
-                },
-                {
-                    type: "input",
-                    name: "roleId",
-                    message: "Enter the employee's role ID",
-                    validate: (answer) => {
-                        if (answer === isNaN) {
-                            return "Employee role ID must be numerical";
-                        } else {
-                    return true; 
-                        }
-                    }
-                },
-                {
-                    type: "input",
-                    name: "managerId",
-                    message: "Enter the employee's manager ID",
-                    validate: (answer) => {
-                        if (answer === isNaN) {
-                            return "Manager ID must be numerical";
-                        } else {
-                    return true; 
-                        }
-                    }
-                },
-            ])
-            .then(function (answer) {
-                connection.query(
-                    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
-                    [answer.firstName, answer.lastName, answer.roleId, answer.managerId],
-                    function (err, res) {
-                        if (err) {
-                            console.log (err)
-                        } 
-                        console.log(
-                            `${answer.firstName} ${answer.lastName} has been added to the team!`
-                        );
+// function addEmployee() {
+//     // Expected Behavior: Enter employee id, first name, last name, salary, dept, + manager then add to db
+//     connection.query('SELECT FROM roles', (err, res) => {
+//         if (err) {
+//             console.log(err)
+//         } else {
+//             return inquirer.prompt([
+//                 {
+//                     type: "input",
+//                     name: "firstName",
+//                     message: "Enter the employee's first name",
+//                     validate: (answer) => {
+//                         if (answer !== "") {
+//                             return true;
+//                         } else {
+//                     return "First name cannot be blank";
+//                         }
+//                     }
+//                 },
+//                 {
+//                     type: "input",
+//                     name: "lastName",
+//                     message: "Enter the employee's last name",
+//                     validate: (answer) => {
+//                         if (answer === "") {
+//                             return true;
+//                         } else {
+//                     return "Last name cannot be blank";
+//                         }
+//                     }
+//                 },
+//                 {
+//                     type: "input",
+//                     name: "roleId",
+//                     message: "Enter the employee's role ID",
+//                     validate: (answer) => {
+//                         if (answer === isNaN) {
+//                             return "Employee role ID must be numerical";
+//                         } else {
+//                     return true; 
+//                         }
+//                     }
+//                 },
+//                 {
+//                     type: "input",
+//                     name: "managerId",
+//                     message: "Enter the employee's manager ID",
+//                     validate: (answer) => {
+//                         if (answer === isNaN) {
+//                             return "Manager ID must be numerical";
+//                         } else {
+//                     return true; 
+//                         }
+//                     }
+//                 },
+//             ])
+//             .then(function (answer) {
+//                 connection.query(
+//                     "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+//                     [answer.firstName, answer.lastName, answer.roleId, answer.managerId],
+//                     function (err, res) {
+//                         if (err) {
+//                             console.log (err)
+//                         } 
+//                         console.log(
+//                             `${answer.firstName} ${answer.lastName} has been added to the team!`
+//                         );
                     
-                        console.log("Employee added successfully!");
-                        init();
-                    }
-                )
-            })
-        }
-    })
-}
+//                         console.log("Employee added successfully!");
+//                         init();
+//                     }
+//                 )
+//             })
+//         }
+//     })
+// }
 
-function updateEmployee() {
-    // Expected Behavior: Enter new employee role, then add to db
+// function updateEmployee() {
+//     // Expected Behavior: Enter new employee role, then add to db
 
-}
-
-init();
+// }
